2e7ccbf6f8d1b4ddea53086666022c363771652d
ZmAleksandra <olazmijewska28@wp.pl>
2020-05-18 21:13:25 +0200

    patient not full menu

--- app/src/main/java/com/example/dentistapplication/pHomeActivity.java
+++ app/src/main/java/com/example/dentistapplication/pHomeActivity.java
@@ -17,13 +17,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class pHomeActivity extends AppCompatActivity {
-
+    TextView userName, userEmail;
     private AppBarConfiguration mAppBarConfiguration;
+    //deklaracja instancji FirebaseAuth, FirebaseUser, FirebaseDatabase i FirebaseReference
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +49,7 @@ public class pHomeActivity extends AppCompatActivity {
         //ustawienie toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //inicjacja instancji FirebaseAuth i FirebaseDatabase
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +58,14 @@ public class pHomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //inicjacja instancji FirebaseAuth i FirebaseDatabase
+        firebaseAuth =  FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        //pobranie aktualnie zalogowanego użytkownika
+        user = firebaseAuth.getCurrentUser();
+        //referencja do ścieżki do tabeli 'Users'
+        databaseReference = firebaseDatabase.getReference("Patients");
+        //Zainicjowanie menu głównego
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -52,6 +77,31 @@ public class pHomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.p_nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        View headerView = navigationView.getHeaderView(0);
+//poniższe pola zostaną wypełnione informacjami o użytkowniku
+        userName = headerView.findViewById(R.id.navName);
+        userEmail = headerView.findViewById(R.id.navEmail);
+
+        //wyszukanie użytkownika po mailu
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    //zapisanie do zmiennych wartości z bazy danych
+                    String nameSurname = "" + ds.child("name").getValue() +" "+ ds.child("surname").getValue();
+                    String email = "" + ds.child("email").getValue();
+
+                    //ustawienie w menu imienia i nazwiska, oraz adresu e-mail
+                    userName.setText(nameSurname);
+                    userEmail.setText(email);
+
+                }}
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
     //dodanie menu po prawej stronie w górnym rogu
     @Override
